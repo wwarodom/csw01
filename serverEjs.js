9c111a1416e20c764864b5655f592b8079ba3e48
warodom.w <warodom.w>
2016-03-22 11:40:27 +0700

    push value to array

--- serverEjs.js
+++ serverEjs.js
@@ -1,8 +1,10 @@
 var express = require('express'),
-		app	= express()
+		app	= express(),
+		bodyParser = require('body-parser')
 
 var posts_json = [{'user':'John','comment':'Hello John' },
 				  {'user':'Jim','comment':'Hello Jim' }]
+var urlencodedParser = bodyParser.urlencoded({extended:false})
 
 app.set('views','./views')		
 app.set('view engine','ejs')
@@ -12,6 +14,18 @@ app.get('/ejs',function(req,res){
 		  message:'How are you doing?',
 		  posts: posts_json} )
 })
+
+app.post('/addUser', urlencodedParser, function(req,res){
+	var length = Object.keys(posts_json).length
+	posts_json.push({})
+	posts_json[length].user = req.body.user
+	posts_json[length].comment = req.body.comment
+	res.render('post', 
+		{ title: 'Hello Jade', 
+		  message:'How are you doing?',
+		  posts: posts_json} )
+})
+
 app.listen(8000, function() {
 	console.log('Server is running...')
 })
