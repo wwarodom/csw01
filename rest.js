faa32bbc4dba09665153162220596cd571f6c309
warodom.w <warodom.w>
2016-03-29 11:49:31 +0700

    Restful nodejs

---
+++ rest.js
@@ -0,0 +1,50 @@
+var express = require('express')
+var app = express() 
+var router = express.Router() 
+var bodyParser = require('body-parser')
+var bears = [{'id':0,'name':'Vinnie','weight':100},
+			 {'id':1,'name':'Pooh','weight':80}] 			  
+router.route('/bears') 
+	// post new bear
+  .post(function(req, res) { 
+    var bear = {}
+    bear.id = req.body.id
+    bear.name = req.body.name 
+    bear.weight = req.body.weight
+    bears.push(bear)
+    res.json(bears) 
+  })
+
+  // get all bears
+  .get(function(req,res) {
+  	res.json(bears)
+  })
+
+router.route('/bears/:bear_id')
+	// show a bear
+	.get(function(req,res) {
+		res.json(bears[req.params.bear_id])
+	})
+
+	// update a bear
+	.put(function(req,res) {
+		var id = req.params.bear_id
+		bears[id].name = req.body.name
+		bears[id].weight = req.body.weight
+		res.json({message: 'Bear updated!'})
+	})
+
+	// delete a bear
+	.delete(function(req,res) {
+		var id = req.params.bear_id
+		delete bears[id]
+		res.json({message: 'Bear deleted!'})
+	})
+
+// all of our routes will be prefixed with /api 
+// app.use('/api', bodyParser.json(), router)
+app.use('/api',bodyParser.urlencoded({extended:false}), router)
+
+app.listen(8000, function (){
+	console.log('Server is running...')
+})
